c361a2f2d11395584dbfd827fad6b0e8c346d258
glooneltharion <gloo.neltharion@gmail.com>
2021-11-04 16:39:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+/* 
+Questions1: 14
+Questions2: 
+let a = 2;
+if (a < 5) {
+  console.log(a / 2);
+} else {
+  console.log(a * 2);
+}
+*/
+
+/*
+for (var i = 0; i <= 9; i++) {
+    console.log(i);
+   }
+*/
+/*
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+   }
+*/
+for (var i = 0; i <= 20; i++) {
+    if( i% 2 == 0){
+        console.log(i);
+       }
+   }
